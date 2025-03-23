903082ecd9c9eecad916bb78bed7fb137ef3e94e
Tom Woodley <thwoodley@gmail.com>
2025-03-23 16:56:18 +0000

    use try monad for fetching api key

--- app/utils/SecretsManagerUtil.scala
+++ app/utils/SecretsManagerUtil.scala
@@ -2,12 +2,15 @@ package utils
 
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
 import software.amazon.awssdk.services.secretsmanager.model.{GetSecretValueRequest, GetSecretValueResponse}
+import scala.util.Try
 
 object SecretsManagerUtil {
-  def getSecret(secretName: String): String = {
-    val client                                         = SecretsManagerClient.builder().build()
-    val getSecretValueRequest                          = GetSecretValueRequest.builder().secretId(secretName).build()
-    val getSecretValueResponse: GetSecretValueResponse = client.getSecretValue(getSecretValueRequest)
-    getSecretValueResponse.secretString()
+  def getSecret(secretName: String): Try[String] = {
+    Try {
+      val client = SecretsManagerClient.builder().build()
+      val getSecretValueRequest = GetSecretValueRequest.builder().secretId(secretName).build()
+      val getSecretValueResponse: GetSecretValueResponse = client.getSecretValue(getSecretValueRequest)
+      getSecretValueResponse.secretString()
+    }
   }
 }
